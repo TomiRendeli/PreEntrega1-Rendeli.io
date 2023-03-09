413c0d4afd4a6fe94ce8523cbb729881c75fd97d
Tomas Rendeli <94577658+TomiRendeli@users.noreply.github.com>
2023-03-09 20:18:33 -0300

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,17 @@
+
+import MainHeader from '@/components/header/MainHeader';
+import MainProducts from './components/product/MainProducts';
+const App = () => { 
+    return (
+    <>
+    <MainHeader />
+    <hr />
+    <MainProducts />
+
+    
+    </>
+    );
+
+};
+
+export default App;
